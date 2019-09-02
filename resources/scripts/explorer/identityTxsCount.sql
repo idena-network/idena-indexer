a0196fe0668887ae97782dcb764f0a1b62ea45fb
mbidenaio <mb@idena.io>
2019-09-03 01:19:31 +0500

    Return incoming txs

--- resources/scripts/explorer/identityTxsCount.sql
+++ resources/scripts/explorer/identityTxsCount.sql
@@ -3,3 +3,4 @@ from transactions t
          join blocks b on b.height = t.block_height
          join addresses afrom on afrom.id = t.from
 where lower(afrom.address) = lower($1)
+   or lower(ato.address) = lower($1)
