fc1a527456441e59aedea983951a2d8b212253ce
mbidenaio <mb@idena.io>
2019-09-16 18:03:19 +0500

    Fix age

--- resources/scripts/explorer/identityAge.sql
+++ resources/scripts/explorer/identityAge.sql
@@ -1,7 +1,16 @@
-select (select max(epoch) from epochs) - coalesce(
-        (select ((select max(epoch) from epochs) - max(b.epoch)) age
-         from address_states s
-                  join blocks b on b.height = s.block_height
-                  join addresses a on a.id = s.address_id
-         where s.state = 'Candidate'
-           and lower(a.address) = lower($1)), 0)
+select case
+           when ((select s.state
+                  from address_states s
+                           join addresses a on a.id = s.address_id
+                  where lower(a.address) = lower($1)
+                    and s.is_actual) not in ('Verified', 'Suspended', 'Zombie', 'Newbie'))
+               then 0
+           else
+                       (select max(epoch) from epochs) -
+                       coalesce((select max(b.epoch) age
+                                 from address_states s
+                                          join blocks b on b.height = s.block_height
+                                          join addresses a on a.id = s.address_id
+                                 where s.state = 'Candidate'
+                                   and lower(a.address) = lower($1)), 0)
+           end
