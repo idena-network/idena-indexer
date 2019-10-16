7f89ddce6e151e776852d44747bf052896e8ebd1
mbidenaio <mb@idena.io>
2019-10-17 01:17:47 +0500

    Fix age query - take into account case when there is no address state records

--- resources/scripts/explorer/identityAge.sql
+++ resources/scripts/explorer/identityAge.sql
@@ -1,14 +1,14 @@
 select case
-           when ((select s.state
-                  from address_states s
-                           join addresses a on a.id = s.address_id
-                  where lower(a.address) = lower($1)
-                    and s.is_actual) not in ('Verified', 'Suspended', 'Zombie', 'Newbie'))
-               then 0
+           when (select s.state
+                 from address_states s
+                          join addresses a on a.id = s.address_id
+                 where lower(a.address) = lower($1)
+                   and s.is_actual) in ('Verified', 'Suspended', 'Zombie', 'Newbie')
+               then (select max(epoch) from epochs) -
+                    coalesce((select bd.birth_epoch
+                              from birthdays bd
+                                       join addresses a on a.id = bd.address_id
+                              where lower(a.address) = lower($1)), 0)
            else
-                       (select max(epoch) from epochs) -
-                       coalesce((select bd.birth_epoch
-                                 from birthdays bd
-                                          join addresses a on a.id = bd.address_id
-                                 where lower(a.address) = lower($1)), 0)
+               0
            end
