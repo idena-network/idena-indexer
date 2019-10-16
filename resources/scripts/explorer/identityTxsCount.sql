77e872961963e48c609e54f44a5d7b821df08805
mbidenaio <mb@idena.io>
2019-10-17 00:54:46 +0500

    Fix identity txs count query

--- resources/scripts/explorer/identityTxsCount.sql
+++ resources/scripts/explorer/identityTxsCount.sql
@@ -2,5 +2,6 @@ select count(*) tx_count
 from transactions t
          join blocks b on b.height = t.block_height
          join addresses afrom on afrom.id = t.from
+         left join addresses ato on ato.id = t.to
 where lower(afrom.address) = lower($1)
    or lower(ato.address) = lower($1)
