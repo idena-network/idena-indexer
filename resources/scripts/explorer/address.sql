c4f7a9f6c974b7cace836032af9aa3361c207813
mbidenaio <mb@idena.io>
2019-10-17 17:33:52 +0500

    Return number of in & out txs

--- resources/scripts/explorer/address.sql
+++ resources/scripts/explorer/address.sql
@@ -1,7 +1,7 @@
 select a.Address,
-       coalesce(b.Balance, '0')                                balance,
-       coalesce(b.Stake, '0')                                  stake,
-       (select count(*) from transactions where "from" = a.id) tx_count
+       coalesce(b.Balance, '0')                                               balance,
+       coalesce(b.Stake, '0')                                                 stake,
+       (select count(*) from transactions where "from" = a.id or "to" = a.id) tx_count
 from addresses a
          left join balances b on b.address_id = a.id
 where lower(a.Address) = lower($1)
