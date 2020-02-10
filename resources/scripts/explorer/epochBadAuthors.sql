7a289cccdca059bac3237a937e2f12d9cc7addf8
mbidenaio <mb@idena.io>
2020-02-10 10:40:27 +0500

    Fix bad authors query

--- resources/scripts/explorer/epochBadAuthors.sql
+++ resources/scripts/explorer/epochBadAuthors.sql
@@ -7,7 +7,7 @@ from bad_authors ba
          join epoch_identities ei on ei.id = ba.epoch_identity_id
          join address_states s on s.id = ei.address_state_id
          join addresses a on a.id = s.address_id
-         left join (select t.from id
+         left join (select distinct t.from id
                     from flips f
                              join transactions t on t.id = f.tx_id
                     where f.wrong_words
