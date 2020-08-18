1ae0986e3479438d8e15c6c4bda603aab0e8cd1e
mbidenaio <mb@idena.io>
2020-08-18 16:00:59 +0500

    Return not killed temporary addresses

--- resources/scripts/explorer/epochIdentityStatesInterimSummary.sql
+++ resources/scripts/explorer/epochIdentityStatesInterimSummary.sql
@@ -4,13 +4,20 @@ from (select eiis.address_state_id
                join blocks b on b.height = eiis.block_height and b.epoch = $1
       where exists(select 1 from epoch_identities where epoch = $1)
       union
-      select s.id
+      select s.id address_state_id
+      from address_states s
+      where $1 - 1 = (select max(epoch) from epoch_identities)
+        and s.is_actual
+        and not s.state in (0, 5)
+      union
+      select s.id address_state_id
       from address_states s
                join blocks b on b.height = s.block_height
                left join temporary_identities ti on ti.address_id = s.address_id
       where $1 - 1 = (select max(epoch) from epoch_identities)
         and s.is_actual
-        and not (s.state in (0, 5) and b.epoch < $1)
+        and s.state in (0, 5)
+        and b.epoch = $1
         and ti.address_id is null) t
          join address_states s on s.id = t.address_state_id
          join dic_identity_states dics on dics.id = s.state
