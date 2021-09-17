2643394c4449926ea2242f915a36bc3a98a4d3d8
mbidenaio <mb@idena.io>
2021-09-17 14:29:53 +0500

    Do not return timestamp of mempool txs

--- core/types/types.go
+++ core/types/types.go
@@ -28,16 +28,15 @@ type UpgradeVotes struct {
 }
 
 type TransactionSummary struct {
-	Hash      string           `json:"hash"`
-	Type      string           `json:"type,omitempty" enums:"SendTx,ActivationTx,InviteTx,KillTx,SubmitFlipTx,SubmitAnswersHashTx,SubmitShortAnswersTx,SubmitLongAnswersTx,EvidenceTx,OnlineStatusTx,KillInviteeTx,ChangeGodAddressTx,BurnTx,ChangeProfileTx,DeleteFlipTx,DeployContract,CallContract,TerminateContract,DelegateTx,UndelegateTx,KillDelegatorTx,StoreToIpfsTx"`
-	Timestamp time.Time        `json:"timestamp" example:"2020-01-01T00:00:00Z"`
-	From      string           `json:"from,omitempty"`
-	To        string           `json:"to,omitempty"`
-	Amount    *decimal.Decimal `json:"amount,omitempty" swaggertype:"string"`
-	Tips      *decimal.Decimal `json:"tips,omitempty" swaggertype:"string"`
-	MaxFee    *decimal.Decimal `json:"maxFee,omitempty" swaggertype:"string"`
-	Fee       *decimal.Decimal `json:"fee,omitempty" swaggertype:"string"`
-	Size      uint32           `json:"size,omitempty"`
+	Hash   string           `json:"hash"`
+	Type   string           `json:"type,omitempty" enums:"SendTx,ActivationTx,InviteTx,KillTx,SubmitFlipTx,SubmitAnswersHashTx,SubmitShortAnswersTx,SubmitLongAnswersTx,EvidenceTx,OnlineStatusTx,KillInviteeTx,ChangeGodAddressTx,BurnTx,ChangeProfileTx,DeleteFlipTx,DeployContract,CallContract,TerminateContract,DelegateTx,UndelegateTx,KillDelegatorTx,StoreToIpfsTx"`
+	From   string           `json:"from,omitempty"`
+	To     string           `json:"to,omitempty"`
+	Amount *decimal.Decimal `json:"amount,omitempty" swaggertype:"string"`
+	Tips   *decimal.Decimal `json:"tips,omitempty" swaggertype:"string"`
+	MaxFee *decimal.Decimal `json:"maxFee,omitempty" swaggertype:"string"`
+	Fee    *decimal.Decimal `json:"fee,omitempty" swaggertype:"string"`
+	Size   uint32           `json:"size,omitempty"`
 	// Deprecated
 	Transfer *decimal.Decimal `json:"transfer,omitempty" swaggerignore:"true"`
 	Data     interface{}      `json:"data,omitempty"`
@@ -51,7 +50,6 @@ type TransactionDetail struct {
 	BlockHash   string          `json:"blockHash"`
 	Hash        string          `json:"hash"`
 	Type        string          `json:"type" enums:"SendTx,ActivationTx,InviteTx,KillTx,SubmitFlipTx,SubmitAnswersHashTx,SubmitShortAnswersTx,SubmitLongAnswersTx,EvidenceTx,OnlineStatusTx,KillInviteeTx,ChangeGodAddressTx,BurnTx,ChangeProfileTx,DeleteFlipTx,DeployContract,CallContract,TerminateContract,DelegateTx,UndelegateTx,KillDelegatorTx,StoreToIpfsTx"`
-	Timestamp   time.Time       `json:"timestamp" example:"2020-01-01T00:00:00Z"`
 	From        string          `json:"from"`
 	To          string          `json:"to,omitempty"`
 	Amount      decimal.Decimal `json:"amount" swaggertype:"string"`
